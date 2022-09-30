ab0f7a2e443c3327ec2896bf84ca815236922000
Kyle Hobdey <kyle.hobdey@hotmail.co.uk>
2022-09-30 13:30:41 +0100

    Created competitor component

---
+++ F1Predictions/F1Predictions.Components/Competitors/Competitor.razor.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Components;
+
+namespace F1Predictions.Components.Competitors
+{
+    public partial class Competitor
+    {
+        [Parameter]
+        public string Initials { get; set; } = string.Empty;
+
+        [Parameter]
+        public string TeamId { get; set; } = string.Empty;
+    }
+}
