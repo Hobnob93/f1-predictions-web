c7d4bbdc16e1adba66aba8da2febf7b563dca41a
Kyle Hobdey <kyle.hobdey@hotmail.co.uk>
2022-10-22 21:28:27 +0100

    Added CORS support to API

--- F1Predictions/F1Predictions.Api/Program.cs
+++ F1Predictions/F1Predictions.Api/Program.cs
@@ -21,6 +21,11 @@ if (app.Environment.IsDevelopment())
     app.UseSwaggerUI();
 }
 
+app.UseCors(opt => opt
+    .AllowAnyOrigin()
+    .WithMethods("GET", "POST", "OPTIONS")
+    .WithHeaders("Authorization", "Content-Type"));
+
 app.UseHttpsRedirection();
 
 app.UseAuthorization();
