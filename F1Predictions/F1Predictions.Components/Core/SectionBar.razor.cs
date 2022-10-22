c02a8064b3d8f67aa82a3c33892856600e94a219
Kyle Hobdey <kyle.hobdey@hotmail.co.uk>
2022-10-22 21:07:48 +0100

    Changed index page to be a loading page + added results page

---
+++ F1Predictions/F1Predictions.Components/Core/SectionBar.razor.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.AspNetCore.Components;
+
+namespace F1Predictions.Components.Core
+{
+    public partial class SectionBar
+    {
+        [Parameter, EditorRequired]
+        public string Title { get; set; } = string.Empty;
+    }
+}
