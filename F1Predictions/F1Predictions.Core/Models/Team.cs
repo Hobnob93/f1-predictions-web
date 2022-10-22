c65a82051958470cddfd30d87b7d53a1c580f3b5
Kyle Hobdey <kyle.hobdey@hotmail.co.uk>
2022-10-22 21:12:53 +0100

    Added team model

---
+++ F1Predictions/F1Predictions.Core/Models/Team.cs
@@ -0,0 +1,10 @@
+﻿namespace F1Predictions.Core.Models
+{
+    public class Team
+    {
+		public string Id { get; set; } = string.Empty;
+		public string Name { get; set; } = string.Empty;
+		public string ImageName { get; set; } = string.Empty;
+		public string Color { get; set; } = string.Empty;
+    }
+}
