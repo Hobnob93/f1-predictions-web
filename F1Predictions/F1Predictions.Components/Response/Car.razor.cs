301e6acaf51c9168c0a1158ff1b3e803606cb879
Kyle Hobdey <kyle.hobdey@hotmail.co.uk>
2022-10-23 13:13:34 +0100

    Using data initializer + teams service for fetching team data for displaying their car

--- F1Predictions/F1Predictions.Components/Response/Car.razor.cs
+++ F1Predictions/F1Predictions.Components/Response/Car.razor.cs
@@ -1,14 +1,20 @@
 ﻿using BlazorComponentUtilities;
+using F1Predictions.Core.Interfaces;
 using Microsoft.AspNetCore.Components;
 
 namespace F1Predictions.Components.Response
 {
     public partial class Car : BaseComponent
     {
+        private const string ImageSourceFormat = "images/2022/cars/{0}.png";
+
+        [Inject]
+        public ITeamsDataService TeamsService { get; set; } = default!;
+
         [Parameter, EditorRequired]
         public string TeamId { get; set; } = string.Empty;
 
-        private string _imageSource => "images/2022/cars/aston-martin.png";
+        private string ImageSource => string.Format(ImageSourceFormat, TeamsService.FindItem(TeamId).ImageName);
 
         public string Classes => new CssBuilder()
             .AddClass("car")
---
+++ F1Predictions/F1Predictions/Pages/Index.razor.cs
@@ -0,0 +1,23 @@
+﻿using F1Predictions.Core.Interfaces;
+using Microsoft.AspNetCore.Components;
+
+namespace F1Predictions.Pages
+{
+    public partial class Index
+    {
+        [Inject]
+        public IDataServicesInitializer DataInitializer { get; set; } = default!;
+
+        [Inject]
+        public NavigationManager NavManager { get; set; } = default!;
+
+        protected override async Task OnAfterRenderAsync(bool firstRender)
+        {
+            await base.OnAfterRenderAsync(firstRender);
+
+            await DataInitializer.InitializeTeamsAsync();
+
+            NavManager.NavigateTo("/result");
+        }
+    }
+}
