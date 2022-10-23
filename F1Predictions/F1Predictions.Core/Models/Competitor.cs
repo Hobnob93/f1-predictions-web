1343d0ce7da4227ebd418fc5ce9c797795b94347
Kyle Hobdey <kyle.hobdey@hotmail.co.uk>
2022-10-23 13:33:48 +0100

    Added competitor model

---
+++ F1Predictions/F1Predictions.Core/Models/Competitor.cs
@@ -0,0 +1,10 @@
+﻿namespace F1Predictions.Core.Models
+{
+    public class Competitor
+    {
+        public string Id { get; set; } = string.Empty;
+        public string Name { get; set; } = string.Empty;
+        public string Nickname { get; set; } = string.Empty;
+        public string LiveryId { get; set; } = string.Empty;
+    }
+}
