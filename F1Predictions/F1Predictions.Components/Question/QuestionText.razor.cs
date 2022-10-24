7e833aceba7989980fb03a1efa58afbd19149419
Kyle Hobdey <kyle.hobdey@hotmail.co.uk>
2022-10-24 16:27:35 +0100

    Added QuestionText component

---
+++ F1Predictions/F1Predictions.Components/Question/QuestionText.razor.cs
@@ -0,0 +1,16 @@
+﻿using BlazorComponentUtilities;
+using Microsoft.AspNetCore.Components;
+
+namespace F1Predictions.Components.Question
+{
+    public partial class QuestionText : BaseComponent
+    {
+        [Parameter, EditorRequired]
+        public string Text { get; set; } = string.Empty;
+
+        public string Classes => new CssBuilder()
+            .AddClass("mt-n4")
+            .AddClass(Class, when: Class is not null)
+            .Build();
+    }
+}
