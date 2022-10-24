ccf29478dcd98a243082100cc5042519e85a92ea
Kyle Hobdey <kyle.hobdey@hotmail.co.uk>
2022-10-24 21:16:22 +0100

    Created intro content 'question'

--- F1Predictions/F1Predictions.Components/Question/QuestionMaster.razor.cs
+++ F1Predictions/F1Predictions.Components/Question/QuestionMaster.razor.cs
@@ -1,7 +1,41 @@
-﻿namespace F1Predictions.Components.Question
-{
-    public partial class QuestionMaster
-    {
+﻿using F1Predictions.Core.Interfaces;
+using Microsoft.AspNetCore.Components;
 
+namespace F1Predictions.Components.Question
+{
+    public partial class QuestionMaster : IDisposable
+    {
+        [Inject]
+        public IQuestionsDataService QuestionsService { get; set; } = default!;
+
+        protected override void OnInitialized()
+        {
+            QuestionsService.StateChanged += QuestionChanged;
+
+            base.OnInitialized();
+        }
+
+        private async Task QuestionChanged()
+        {
+            await InvokeAsync(StateHasChanged);
+        }
+
+        private Type GetCurrentQuestionType()
+        {
+            if (QuestionsService.CurrentQuestion is null)
+                throw new InvalidOperationException("The current question is null!");
+
+            var scoringType = QuestionsService.CurrentQuestion.Scoring;
+            return scoringType switch
+            {
+                "INTRO" => typeof(IntroContent),
+                _ => throw new InvalidOperationException($"The question type {scoringType} is not recognised.")
+            };
+        }
+
+        public void Dispose()
+        {
+            QuestionsService.StateChanged -= QuestionChanged;
+        }
     }
 }
--- F1Predictions/F1Predictions/Pages/Results.razor.cs
+++ F1Predictions/F1Predictions/Pages/Results.razor.cs
@@ -1,40 +1,7 @@
-﻿using F1Predictions.Core.Interfaces;
-using F1Predictions.Core.Models;
-using Microsoft.AspNetCore.Components;
-using MudBlazor;
-
-namespace F1Predictions.Pages
+﻿namespace F1Predictions.Pages
 {
     public partial class Results
     {
-        [Inject]
-        public ICompetitorsDataService CompetitorsService { get; set; } = default!;
-
-        [Inject]
-        public ITeamsDataService TeamsService { get; set; } = default!;
-
-        public List<ChartDataPoint> FavouriteLiveriesData { get; private set; } = new();
-        public ChartOptions ChartOptions { get; private set; } = new();
-
-        protected override void OnInitialized()
-        {
-            SetFavouriteLiveries();
-
-            base.OnInitialized();
-        }
-
-        private void SetFavouriteLiveries()
-        {
-            var liveryIds = CompetitorsService.Data.Select(d => d.LiveryId);
-            var liveryNames = liveryIds.Select(id => TeamsService.FindItem(id).Name).ToList();
-
-            ChartOptions.ChartPalette = TeamsService.Data.Select(t => t.Color).ToArray();
-
-            FavouriteLiveriesData = TeamsService.Data.Select(t => new ChartDataPoint
-            {
-                Name = t.Name,
-                Value = liveryNames.Count(ln => ln == t.Name)
-            }).ToList();
-        }
+        
     }
 }
