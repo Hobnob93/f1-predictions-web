346b2be89ad3f30ee6cec6d143a414376034a142
Kyle Hobdey <kyle.hobdey@hotmail.co.uk>
2022-10-25 14:56:20 +0100

    Added competitor responses component that contains a competitor template

---
+++ F1Predictions/F1Predictions.Components/Response/CompetitorResponses.razor.cs
@@ -0,0 +1,21 @@
+﻿using BlazorComponentUtilities;
+using F1Predictions.Core.Interfaces;
+using Microsoft.AspNetCore.Components;
+using CompetitorData = F1Predictions.Core.Models.Competitor;
+
+namespace F1Predictions.Components.Response
+{
+    public partial class CompetitorResponses : BaseComponent
+    {
+        [Inject]
+        public ICompetitorsDataService CompetitorsService { get; set; } = default!;
+
+        [Parameter, EditorRequired]
+        public RenderFragment<CompetitorData> CompetitorTemplate { get; set; } = default!;
+
+        public string Classes => new CssBuilder()
+            .AddClass("mt-4")
+            .AddClass(Class, when: Class is not null)
+            .Build();
+    }
+}
