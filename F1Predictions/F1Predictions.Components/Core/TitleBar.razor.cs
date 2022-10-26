f92ea6ff05391cc5d31d07abad8e66d2fcc1f035
Kyle Hobdey <kyle.hobdey@hotmail.co.uk>
2022-10-26 19:16:47 +0100

    Enable/disable back/forward button if they can't go forward or back anymore

---
+++ F1Predictions/F1Predictions.Components/Core/TitleBar.razor.cs
@@ -0,0 +1,28 @@
+﻿using F1Predictions.Core.Interfaces;
+using Microsoft.AspNetCore.Components;
+
+namespace F1Predictions.Components.Core
+{
+    public partial class TitleBar : IDisposable
+    {
+        [Inject]
+        private IQuestionsDataService QuestionsService { get; set; } = default!;
+
+        protected override async Task OnInitializedAsync()
+        {
+            QuestionsService.StateChanged += OnQuestionChangedAsync;
+
+            await base.OnInitializedAsync();
+        }
+
+        private async Task OnQuestionChangedAsync()
+        {
+            await InvokeAsync(StateHasChanged);
+        }
+
+        public void Dispose()
+        {
+            QuestionsService.StateChanged -= OnQuestionChangedAsync;
+        }
+    }
+}
