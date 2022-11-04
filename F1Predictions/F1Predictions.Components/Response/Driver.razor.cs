afc61d12e16041ad4c6194eb3b3b3652471f91d1
Kyle Hobdey <kyle.hobdey@hotmail.co.uk>
2022-11-04 14:53:47 +0000

    Added Driver component

---
+++ F1Predictions/F1Predictions.Components/Response/Driver.razor.cs
@@ -0,0 +1,34 @@
+﻿using BlazorComponentUtilities;
+using F1Predictions.Core.Interfaces;
+using Microsoft.AspNetCore.Components;
+
+namespace F1Predictions.Components.Response
+{
+    public partial class Driver : BaseComponent
+    {
+        private const string ImageSourceFormat = "images/2022/drivers/{0}.png";
+
+        [Inject]
+        public IDriversDataService DriversService { get; set; } = default!;
+
+        [Parameter, EditorRequired]
+        public string DriverId { get; set; } = string.Empty;
+
+        private F1Predictions.Core.Models.Driver TargetDriver => DriversService.FindItem(DriverId);
+        private string ImageSource => string.Format(ImageSourceFormat, TargetDriver.ImageName);
+        private string DriverColor => TargetDriver.Color;
+
+        private string Classes => new CssBuilder()
+            .AddClass("team")
+            .AddClass("breathe")
+            .AddClass(Class, when: Class is not null)
+            .Build();
+
+        private string ColorStyles => new StyleBuilder()
+            .AddStyle("border-color", DriverColor)
+            .AddStyle("color", DriverColor)
+            .Build();
+
+        private string Styles => $"{ColorStyles} {Style}";
+    }
+}
