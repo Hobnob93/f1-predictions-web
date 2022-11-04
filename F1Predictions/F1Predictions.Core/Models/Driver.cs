c12b1b4b9550f11aab3c310cb8a35d23047eba9b
Kyle Hobdey <kyle.hobdey@hotmail.co.uk>
2022-11-04 14:11:56 +0000

    Added driver model

---
+++ F1Predictions/F1Predictions.Core/Models/Driver.cs
@@ -0,0 +1,10 @@
+﻿namespace F1Predictions.Core.Models
+{
+    public class Driver
+    {
+        public string Id { get; set; } = string.Empty;
+        public string FirstName { get; set; } = string.Empty;
+        public string LastName { get; set; } = string.Empty;
+        public string TeamId { get; set; } = string.Empty;
+    }
+}
