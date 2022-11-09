13163412354b2ef67d703aa52176fd321830f65e
Kyle Hobdey <kyle.hobdey@hotmail.co.uk>
2022-11-09 19:51:14 +0000

    Implemented new pie chart component

---
+++ F1Predictions/F1Predictions.Components/Core/PieChart.razor.cs
@@ -0,0 +1,17 @@
+﻿using ApexCharts;
+using F1Predictions.Core.Models;
+using Microsoft.AspNetCore.Components;
+
+namespace F1Predictions.Components.Core
+{
+    public partial class PieChart : BaseComponent
+    {
+        [Parameter, EditorRequired]
+        public string Title { get; set; } = string.Empty;
+
+        [Parameter, EditorRequired]
+        public IEnumerable<ChartDataPoint> Data { get; set; } = default!;
+
+        private ApexChart<ChartDataPoint>? Chart;
+    }
+}
--- F1Predictions/F1Predictions.Core/Models/ChartDataPoint.cs
+++ F1Predictions/F1Predictions.Core/Models/ChartDataPoint.cs
@@ -4,6 +4,6 @@
     {
         public string Id { get; set; } = string.Empty;
         public string Name { get; set; } = string.Empty;
-        public double Value { get; set; }
+        public decimal Value { get; set; }
     }
 }
