f902fb08d79ce58b66d4a90bff11b3ebe66e0b18
Kyle Hobdey <kyle.hobdey@hotmail.co.uk>
2022-11-12 09:00:53 +0000

    Added BarChart component for multi team and multi driver questions

---
+++ F1Predictions/F1Predictions.Components/Core/BarChart.razor.cs
@@ -0,0 +1,41 @@
+﻿using ApexCharts;
+using F1Predictions.Core.Models;
+using Microsoft.AspNetCore.Components;
+
+namespace F1Predictions.Components.Core
+{
+    public partial class BarChart : BaseComponent
+    {
+        [Parameter, EditorRequired]
+        public string Title { get; set; } = string.Empty;
+
+        [Parameter, EditorRequired]
+        public List<ChartDataPoint> Data { get; set; } = default!;
+
+        [Parameter, EditorRequired]
+        public EventCallback<ChartDataPoint> SelectionCallback { get; set; }
+
+        private ApexChart<ChartDataPoint>? Chart;
+        private ApexChartOptions<ChartDataPoint> Options { get; set; } = new();
+
+        protected override void OnInitialized()
+        {
+            Options.Tooltip = new Tooltip
+            {
+                Style = new TooltipStyle
+                {
+                    FontSize = "14px"
+                },
+                Theme = "dark"
+            };
+        }
+
+        private async Task OnChartItemSelected(SelectedData<ChartDataPoint> selectedData)
+        {
+            var index = selectedData.DataPointIndex;
+            var item = Data[index];
+
+            await SelectionCallback.InvokeAsync(item);
+        }
+    }
+}
