7cdae4978842281b9674452e031a26e7ff770ae4
Kyle Hobdey <kyle.hobdey@hotmail.co.uk>
2022-11-13 19:41:55 +0000

    Tracks response component

---
+++ F1Predictions/F1Predictions.Components/Response/Tracks.razor.cs
@@ -0,0 +1,21 @@
+﻿using BlazorComponentUtilities;
+using Microsoft.AspNetCore.Components;
+
+namespace F1Predictions.Components.Response
+{
+    public partial class Tracks
+    {
+        [Parameter, EditorRequired]
+        public List<string> TrackIds { get; set; } = new();
+
+        private string Classes => new CssBuilder()
+            .AddClass(Class, when: Class is not null)
+            .Build();
+
+        private string ContainerStyle => new StyleBuilder()
+            .AddStyle("display", "flex")
+            .Build();
+
+        private string Styles => $"{Style} {ContainerStyle}";
+    }
+}
