81cb3133ba16ba265cc7586fe00d4000615bf4b0
Kyle Hobdey <kyle.hobdey@hotmail.co.uk>
2022-11-13 19:01:36 +0000

    Created Track model

---
+++ F1Predictions/F1Predictions.Core/Models/Track.cs
@@ -0,0 +1,9 @@
+﻿namespace F1Predictions.Core.Models
+{
+    public class Track
+    {
+        public string Id { get; set; } = string.Empty;
+        public string Name { get; set; } = string.Empty;
+        public string Country { get; set; } = string.Empty;
+    }
+}
