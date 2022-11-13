b20c3dafc526a3af193d393e915f97b4011910da
Kyle Hobdey <kyle.hobdey@hotmail.co.uk>
2022-11-13 22:11:00 +0000

    Finalising multitrack screen

--- F1Predictions/F1Predictions.Core/Models/Track.cs
+++ F1Predictions/F1Predictions.Core/Models/Track.cs
@@ -3,7 +3,6 @@
     public class Track : DataItem
     {
         public string Name { get; set; } = string.Empty;
-        public string Country { get; set; } = string.Empty;
         public string Color { get; set; } = string.Empty;
     }
 }
