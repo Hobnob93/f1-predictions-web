1aade934ac1024a9dbf3a8ba4e39c78e7ab5a36b
Kyle Hobdey <kyle.hobdey@hotmail.co.uk>
2022-11-17 21:39:08 +0000

    Finally resolved styling for question 3-C

--- F1Predictions/F1Predictions.Components/Question/MultiDriverTrackContent.razor.cs
+++ F1Predictions/F1Predictions.Components/Question/MultiDriverTrackContent.razor.cs
@@ -1,10 +1,11 @@
-﻿using F1Predictions.Core.Interfaces;
+﻿using BlazorComponentUtilities;
+using F1Predictions.Core.Interfaces;
 using F1Predictions.Core.Models;
 using Microsoft.AspNetCore.Components;
 
 namespace F1Predictions.Components.Question
 {
-    public partial class MultiDriverTrack : QuestionContent
+    public partial class MultiDriverTrackContent : QuestionContent
     {
         [Inject]
         private IDriversDataService DriversService { get; set; } = default!;
@@ -43,6 +44,32 @@ namespace F1Predictions.Components.Question
                 .ToList();
 
             return answerIds;
+        }
+
+        private string GetDriverTrackContainerClasses(bool compIsRightAligned)
+        {
+            return new CssBuilder()
+                .AddClass("d-flex")
+                .AddClass("flex-row", when: !compIsRightAligned)
+                .AddClass("flex-row-reverse", when: compIsRightAligned)
+                .AddClass("align-center")
+                .Build();
+        }
+
+        private string GetDriverClasses(bool compIsRightAligned)
+        {
+            return new CssBuilder()
+                .AddClass("ms-n4", when: !compIsRightAligned)
+                .AddClass("me-n4-5", when: compIsRightAligned)
+                .Build();
+        }
+
+        private string GetTrackClasses(bool compIsRightAligned)
+        {
+            return new CssBuilder()
+                .AddClass("ms-n12", when: !compIsRightAligned)
+                .AddClass("me-n9", when: compIsRightAligned)
+                .Build();
         }
     }
 }
