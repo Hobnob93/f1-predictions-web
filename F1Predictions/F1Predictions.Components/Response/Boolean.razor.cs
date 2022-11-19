9514ca131ef7b7464158c0eaa79bb5b026ec5473
Kyle Hobdey <kyle.hobdey@hotmail.co.uk>
2022-11-19 18:16:25 +0000

    Implemented boolean response component

---
+++ F1Predictions/F1Predictions.Components/Response/Boolean.razor.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.AspNetCore.Components;
+
+namespace F1Predictions.Components.Response
+{
+    public partial class Boolean : BaseComponent
+    {
+        [Parameter, EditorRequired]
+        public bool State { get; set; }
+    }
+}
