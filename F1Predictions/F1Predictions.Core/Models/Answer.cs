da831c9dae7f34eb638970337d5e8ca6dbe4782f
Kyle Hobdey <kyle.hobdey@hotmail.co.uk>
2022-11-27 12:01:36 +0000

    Implemented Answer data model

---
+++ F1Predictions/F1Predictions.Core/Models/Answer.cs
@@ -0,0 +1,8 @@
+﻿namespace F1Predictions.Core.Models
+{
+    public class Answer : DataItem
+    {
+        public string? RawAnswer { get; set; }
+        public List<AnswerData> AnswersData { get; set; } = new();
+    }
+}
---
+++ F1Predictions/F1Predictions.Core/Models/AnswerData.cs
@@ -0,0 +1,8 @@
+﻿namespace F1Predictions.Core.Models
+{
+    public class AnswerData
+    {
+        public string Id { get; set; } = string.Empty;
+        public string Value { get; set; } = string.Empty;
+    }
+}
