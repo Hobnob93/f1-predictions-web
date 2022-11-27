369cdbeba9cfd80c2e946bb822ddca2877539a2e
Kyle Hobdey <kyle.hobdey@hotmail.co.uk>
2022-11-27 10:55:59 +0000

    Updated scoring data model

---
+++ F1Predictions/F1Predictions.Core/Enums/ScoringMode.cs
@@ -0,0 +1,9 @@
+﻿namespace F1Predictions.Core.Enums
+{
+    public enum ScoringMode
+    {
+        Tracks,
+        Drivers,
+        Teams
+    }
+}
--- F1Predictions/F1Predictions.Core/Models/ScoringData.cs
+++ F1Predictions/F1Predictions.Core/Models/ScoringData.cs
@@ -6,6 +6,7 @@ namespace F1Predictions.Core.Models
     {
         public ScoringType Type { get; set; } = ScoringType.None;
         public string AnswersId { get; set; } = string.Empty;
-        public string ExtraData { get; set; } = string.Empty;
+        public int? Index { get; set; }
+        public ScoringMode? Mode { get; set; }
     }
 }
