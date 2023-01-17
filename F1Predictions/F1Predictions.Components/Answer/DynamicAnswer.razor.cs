f9e10c2b83d8d0bc5f7defc40ee2610409a1381d
Kyle Hobdey <kyle.hobdey@hotmail.co.uk>
2023-01-17 16:46:05 +0000

    Answers list

--- F1Predictions/F1Predictions.Components/Answer/DynamicAnswer.razor.cs
+++ F1Predictions/F1Predictions.Components/Answer/DynamicAnswer.razor.cs
@@ -24,7 +24,8 @@ namespace F1Predictions.Components.Answer
 
         public F1Predictions.Core.Models.Answer Answer { get; set; } = default!;
         public IRefreshable? AnswersChart { get; set; }
-        public List<ChartDataPoint> AnswerData { get; protected set; } = new();
+        public List<ChartDataPoint> ChartData { get; protected set; } = new();
+        public List<ListDataPoint> ListData { get; protected set; } = new();
 
 
         protected override void OnInitialized()
@@ -57,42 +58,71 @@ namespace F1Predictions.Components.Answer
                 case RenderType.Bar when Answer.Mode != ScoringMode.DriverTracks:
                     DetermineChartData();
                     break;
+                case RenderType.List:
+                    DetermineListData();
+                    break;
             }
+        }
+
+        private void DetermineListData()
+        {
+            var rawItemIds = Answer.RawAnswer?.Split(",") ?? Enumerable.Empty<string>();
+
+            ListData = (Answer.Mode switch
+            {
+                ScoringMode.Teams => rawItemIds
+                    .Select(id => Teams.FindItem(id))
+                    .Select(team => new ListDataPoint
+                    {
+                        Id = team.Id,
+                        Color = team.Color,
+                        Name = team.Name
+                    }),
+                ScoringMode.Drivers => rawItemIds
+                    .Select(id => Drivers.FindItem(id))
+                    .Select(driver => new ListDataPoint
+                    {
+                        Id = driver.Id,
+                        Color = driver.Color,
+                        Name = $"{driver.FirstName} {driver.LastName}"
+                    }),
+                _ => Enumerable.Empty<ListDataPoint>()
+            }).ToList();
         }
 
         private void DetermineChartData()
         {
-            AnswerData = (Answer.Mode switch
+            ChartData = (Answer.Mode switch
             {
                 ScoringMode.Teams => Answer.AnswersData
-                    .Select(ad => (ad, Teams.FindItem(ad.Id)))
+                    .Select(ad => (Data: ad, Team: Teams.FindItem(ad.Id)))
                     .Select((item, index) => new ChartDataPoint
                     {
-                        Id = item.Item2.Id,
+                        Id = item.Team.Id,
                         Index = index,
-                        Color = item.Item2.Color,
-                        Name = item.Item2.Name,
-                        Value = decimal.Parse(item.ad.Value)
+                        Color = item.Team.Color,
+                        Name = item.Team.Name,
+                        Value = decimal.Parse(item.Data.Value)
                     }),
                 ScoringMode.Drivers => Answer.AnswersData
-                    .Select(ad => (ad, Drivers.FindItem(ad.Id)))
+                    .Select(ad => (Data: ad, Driver: Drivers.FindItem(ad.Id)))
                     .Select((item, index) => new ChartDataPoint
                     {
-                        Id = item.Item2.Id,
+                        Id = item.Driver.Id,
                         Index = index,
-                        Color = item.Item2.Color,
-                        Name = item.Item2.LastName,
-                        Value = decimal.Parse(item.ad.Value)
+                        Color = item.Driver.Color,
+                        Name = item.Driver.LastName,
+                        Value = decimal.Parse(item.Data.Value)
                     }),
                 ScoringMode.Tracks => Answer.AnswersData
-                    .Select(ad => (ad, Tracks.FindItem(ad.Id)))
+                    .Select(ad => (Data: ad, Track: Tracks.FindItem(ad.Id)))
                     .Select((item, index) => new ChartDataPoint
                     {
-                        Id = item.Item2.Id,
+                        Id = item.Track.Id,
                         Index = index,
-                        Color = item.Item2.Color,
-                        Name = item.Item2.Name,
-                        Value = decimal.Parse(item.ad.Value)
+                        Color = item.Track.Color,
+                        Name = item.Track.Name,
+                        Value = decimal.Parse(item.Data.Value)
                     }),
                 _ => Enumerable.Empty<ChartDataPoint>()
             }).ToList();
---
+++ F1Predictions/F1Predictions.Core/Models/ListDataPoint.cs
@@ -0,0 +1,9 @@
+﻿namespace F1Predictions.Core.Models
+{
+    public class ListDataPoint
+    {
+        public string Id { get; set; } = string.Empty;
+        public string Name { get; set; } = string.Empty;
+        public string Color { get; set; } = "#CFCFCF";
+    }
+}
