6d6c3f193e96bb8ef71487ad5792f6fd18c820a3
Kyle Hobdey <kyle.hobdey@hotmail.co.uk>
2023-12-16 15:05:04 +0000

    Circuits controller

---
+++ FormulaPredictions/FormulaPredictions.Shared/Models/Circuit.cs
@@ -0,0 +1,9 @@
+﻿using FormulaPredictions.Shared.Models.Base;
+
+namespace FormulaPredictions.Shared.Models;
+
+public class Circuit : BaseItem
+{
+    public string Name { get; set; } = string.Empty;
+    public string Color { get; set; } = string.Empty;
+}
