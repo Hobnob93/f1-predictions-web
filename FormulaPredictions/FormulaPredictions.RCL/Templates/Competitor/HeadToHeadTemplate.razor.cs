6e9b112f5e5899991809b74043f40e45df4e1440
Kyle Hobdey <kyle.hobdey@hotmail.co.uk>
2023-12-28 14:31:14 +0000

    head to head question template

---
+++ FormulaPredictions/FormulaPredictions.RCL/Templates/Competitor/HeadToHeadTemplate.razor.cs
@@ -0,0 +1,34 @@
+using BlazorComponentUtilities;
+using FormulaPredictions.Shared.Models;
+
+namespace FormulaPredictions.RCL.Templates.Competitor;
+
+public partial class HeadToHeadTemplate : BaseTemplateComponent
+{
+    private Driver[]? _drivers;
+    private Driver[] Drivers
+    {
+        get => _drivers ??= GetResponsesForCompetitor<Driver>();
+    }
+
+    private Driver QualiChoice => Drivers.First();
+    private Driver RaceChoice => Drivers.Last();
+    private bool CompIsRightAligned => Competitor.IsRightAligned;
+
+    private string ContainerClasses => new CssBuilder()
+        .AddClass("d-flex")
+        .AddClass("flex-row", when: !CompIsRightAligned)
+        .AddClass("flex-row-reverse", when: CompIsRightAligned)
+        .AddClass("align-end")
+        .Build();
+
+    private string DriverClasses => new CssBuilder()
+        .AddClass("ms-n4", when: !CompIsRightAligned)
+        .AddClass("me-n4", when: CompIsRightAligned)
+        .Build();
+
+    private string TextClasses => new CssBuilder()
+        .AddClass("ms-n12", when: !CompIsRightAligned)
+        .AddClass("me-n9", when: CompIsRightAligned)
+        .Build();
+}
