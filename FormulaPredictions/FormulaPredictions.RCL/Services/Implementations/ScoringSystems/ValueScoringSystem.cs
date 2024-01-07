b89f6a12712a8993d49d2abf5b7e00a02672bb5c
Kyle Hobdey <kyle.hobdey@hotmail.co.uk>
2024-01-07 15:19:58 +0000

    Implemented value scoring system

---
+++ FormulaPredictions/FormulaPredictions.RCL/Services/Implementations/ScoringSystems/ValueScoringSystem.cs
@@ -0,0 +1,24 @@
+﻿using FormulaPredictions.RCL.Services.Interfaces;
+using FormulaPredictions.Shared.Models;
+using FormulaPredictions.Shared.State;
+
+namespace FormulaPredictions.RCL.Services.Implementations.ScoringSystems;
+
+public class ValueScoringSystem : BaseScoringSystem, IScoreSystem
+{
+    public ValueScoringSystem(IResponsesService responsesService)
+        : base(responsesService)
+    {
+    }
+
+    public double CalculateScoreForCompetitor(Competitor competitor, AppData appData, CurrentData current)
+    {
+        var answerData = GetAnswerData(appData, current);
+        var answer = int.Parse(answerData.RawAnswer 
+            ?? throw new InvalidOperationException($"Answer for '{answerData.Id}' has not been provided!"));
+
+        var response = _responsesService.GetValueResponse<int>(competitor.Id, current);
+        var score = 25 - Math.Abs(answer - response);
+        return score < 0 ? 0 : score;
+    }
+}
