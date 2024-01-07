bdb52d65ee06db1de3ee7486ad5b5b7638657f1e
Kyle Hobdey <kyle.hobdey@hotmail.co.uk>
2024-01-07 13:43:58 +0000

    Implemented gain-lose scoring system

---
+++ FormulaPredictions/FormulaPredictions.RCL/Services/Implementations/ScoringSystems/GainLoseScoringSystem.cs
@@ -0,0 +1,52 @@
+﻿using FormulaPredictions.RCL.Services.Interfaces;
+using FormulaPredictions.Shared.Models;
+using FormulaPredictions.Shared.State;
+
+namespace FormulaPredictions.RCL.Services.Implementations.ScoringSystems;
+
+public class GainLoseScoringSystem : BaseScoringSystem, IScoreSystem
+{
+    public GainLoseScoringSystem(IResponsesService responsesService)
+        : base(responsesService)
+    {
+    }
+
+    public double CalculateScoreForCompetitor(Competitor competitor, AppData appData, CurrentData current)
+    {
+        var (answerData, responses) = GetAnswerAndResponseItems(competitor, appData, current);
+        if (answerData.AnswersData.Length == 0)
+        {
+            var rawAnswers = answerData.RawAnswer?.Split(",");
+
+            if (rawAnswers is null || rawAnswers.Length == 0)
+                throw new InvalidOperationException($"AnswersData for '{answerData.Id}' has not been provided!"); ;
+
+            answerData.AnswersData = rawAnswers
+                .Select((rr, i) => new AnswerData { Id = rr, Value = i.ToString() })
+                .ToArray();
+        }
+
+        var score = 0;
+        foreach (var response in responses)
+        {
+            if (answerData.AnswersData.Any(ad => ad.Id == response.Id))
+            {
+                score += current.Question.Scoring.Value;
+            }
+            else
+            {
+                score += current.Question.Scoring.ExtraValue;
+            }
+        }
+
+        foreach (var answer in answerData.AnswersData)
+        {
+            if (responses.Any(r => r.Id == answer.Id))
+                continue;
+
+            score += current.Question.Scoring.ExtraValue;
+        }
+
+        return score < 0 ? 0 : score;
+    }
+}
