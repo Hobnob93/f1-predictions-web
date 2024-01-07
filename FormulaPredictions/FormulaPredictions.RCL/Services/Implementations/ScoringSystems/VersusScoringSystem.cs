16b131bda9ac9523346db2cabc361cf80dc8d465
Kyle Hobdey <kyle.hobdey@hotmail.co.uk>
2024-01-07 15:21:44 +0000

    Implemented versus scoring system

---
+++ FormulaPredictions/FormulaPredictions.RCL/Services/Implementations/ScoringSystems/VersusScoringSystem.cs
@@ -0,0 +1,21 @@
+﻿using FormulaPredictions.RCL.Services.Interfaces;
+using FormulaPredictions.Shared.Models;
+using FormulaPredictions.Shared.State;
+
+namespace FormulaPredictions.RCL.Services.Implementations.ScoringSystems;
+
+public class VersusScoringSystem : BaseScoringSystem, IScoreSystem
+{
+    public VersusScoringSystem(IResponsesService responsesService)
+        : base(responsesService)
+    {
+    }
+
+    public double CalculateScoreForCompetitor(Competitor competitor, AppData appData, CurrentData current)
+    {
+        var (answerData, responses) = GetAnswerAndResponseItems(competitor, appData, current);
+        var response = responses.First();
+
+        return response.Id == answerData.AnswersData.First().Id ? 10 : 0;
+    }
+}
