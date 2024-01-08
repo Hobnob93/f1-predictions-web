85604c303f50a38cd4e605e1f3277e5df2c95a27
Kyle Hobdey <kyle.hobdey@hotmail.co.uk>
2024-01-08 20:58:24 +0000

    Implemented scores manager

--- FormulaPredictions/FormulaPredictions.RCL/Services/Implementations/ScoreManager.cs
+++ FormulaPredictions/FormulaPredictions.RCL/Services/Implementations/ScoreManager.cs
@@ -1,4 +1,6 @@
 ﻿using FormulaPredictions.RCL.Services.Interfaces;
+using FormulaPredictions.Shared.Models;
+using FormulaPredictions.Shared.State;
 
 namespace FormulaPredictions.RCL.Services.Implementations;
 
@@ -6,8 +8,77 @@ public class ScoreManager : IScoreManager
 {
     private readonly IScoringSystemFactory _scoreSystemFactory;
 
+    private Dictionary<string, double> _scoresMap = [];
+
     public ScoreManager(IScoringSystemFactory scoreSystemFactory)
     {
         _scoreSystemFactory = scoreSystemFactory;
+    }
+
+    public void CalculateAllScores(AppData appData)
+    {
+        _scoresMap.Clear();
+
+        foreach (var competitor in appData.Competitors)
+        {
+            _scoresMap.Add(competitor.Id, 0d);
+        }
+
+        var fakeCurrent = new CurrentData(new QuestionResponses(), [], false);
+        foreach (var question in appData.Questions)
+        {
+            fakeCurrent = fakeCurrent with
+            {
+                Question = question
+            };
+
+            var scoreSystem = _scoreSystemFactory.CreateSystemFromScoreType(question.Scoring.Type);
+            foreach (var competitor in appData.Competitors)
+            {
+                var score = scoreSystem.CalculateScoreForCompetitor(competitor, appData, fakeCurrent);
+                _scoresMap[competitor.Id] = _scoresMap[competitor.Id] + score;
+            }
+        }
+    }
+
+    public (double Score, Competitor[] Scorers) GetHighestScorers(AppData appData, CurrentData current)
+    {
+        var scoreSystem = _scoreSystemFactory.CreateSystemFromScoreType(current.Question.Scoring.Type);
+
+        var highestScore = 0d;
+        var highestScorers = new List<Competitor>();
+        foreach (var competitor in appData.Competitors)
+        {
+            var score = scoreSystem.CalculateScoreForCompetitor(competitor, appData, current);
+
+            if (score == 0d)
+                continue;
+
+            if (score == highestScore)
+            {
+                highestScorers.Add(competitor);
+                continue;
+            }
+
+            if (score > highestScore)
+            {
+                highestScore = score;
+                highestScorers.Clear();
+                highestScorers.Add(competitor);
+            }
+        }
+
+        return (highestScore, highestScorers.ToArray());
+    }
+
+    public double GetScoreForCompetitor(string competitorId)
+    {
+        if (_scoresMap.Count == 0)
+            throw new Exception($"No scores tracked - please call '{nameof(CalculateAllScores)}' first");
+
+        if (!_scoresMap.TryGetValue(competitorId, out var score))
+            throw new Exception($"Competitor ID '{competitorId}' has no score!");
+
+        return score;
     }
 }
--- FormulaPredictions/FormulaPredictions.RCL/Services/Interfaces/IScoreManager.cs
+++ FormulaPredictions/FormulaPredictions.RCL/Services/Interfaces/IScoreManager.cs
@@ -1,5 +1,11 @@
-﻿namespace FormulaPredictions.RCL.Services.Interfaces;
+﻿using FormulaPredictions.Shared.Models;
+using FormulaPredictions.Shared.State;
+
+namespace FormulaPredictions.RCL.Services.Interfaces;
 
 public interface IScoreManager
 {
+    (double Score, Competitor[] Scorers) GetHighestScorers(AppData appData, CurrentData current);
+    void CalculateAllScores(AppData appData);
+    double GetScoreForCompetitor(string competitorId);
 }
