980687b3d73b67f8e1ca5284d64e0d4d833d9fd2
Kyle Hobdey <kyle.hobdey@hotmail.co.uk>
2024-01-27 14:58:54 +0000

    Head to head scoring bug fix

--- FormulaPredictions/FormulaPredictions.RCL/Services/Implementations/ScoringSystems/HeadToHeadScoringSystem.cs
+++ FormulaPredictions/FormulaPredictions.RCL/Services/Implementations/ScoringSystems/HeadToHeadScoringSystem.cs
@@ -1,5 +1,6 @@
 ﻿using FormulaPredictions.RCL.Services.Interfaces;
 using FormulaPredictions.Shared.Models;
+using FormulaPredictions.Shared.Models.Base;
 using FormulaPredictions.Shared.State;
 
 namespace FormulaPredictions.RCL.Services.Implementations.ScoringSystems;
@@ -15,10 +16,26 @@ public class HeadToHeadScoringSystem : BaseScoringSystem, IScoringSystem
     {
         var (answerData, responses) = GetAnswerAndResponseItems(competitor, appData, current);
 
-        var score = 0;
-        score += responses.First().Id == answerData.AnswersData[0].Id ? 10 : 0;
-        score += responses.Last().Id == answerData.AnswersData[2].Id ? 10 : 0;
+        var score = 0d;
+        score += GetQualiScore(responses, answerData);
+        score += GetRacingScore(responses, answerData);
 
         return score;
+    }
+
+    private double GetQualiScore(BaseItem[] responses, Answer answerData)
+    {
+        if (answerData.AnswersData[0].Value == answerData.AnswersData[1].Value)
+            return 10;
+
+        return responses.First().Id == answerData.AnswersData[0].Id ? 10 : 0;
+    }
+
+    private double GetRacingScore(BaseItem[] responses, Answer answerData)
+    {
+        if (answerData.AnswersData[2].Value == answerData.AnswersData[3].Value)
+            return 10;
+
+        return responses.Last().Id == answerData.AnswersData[2].Id ? 10 : 0;
     }
 }
